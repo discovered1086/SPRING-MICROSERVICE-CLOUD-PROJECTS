43a10900a33e27f333a13e921ba27e41808efd2c
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-03-02 21:05:01 -0600

    Microservice project to follow along with Josh Long

---
+++ FollowingAlongWithJoshLong/src/main/java/com/kingshuk/microservices/followingjoshlong/FollowingAlongWithJoshLongApplication.java
@@ -0,0 +1,13 @@
+package com.kingshuk.microservices.followingjoshlong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowingAlongWithJoshLongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowingAlongWithJoshLongApplication.class, args);
+	}
+
+}
