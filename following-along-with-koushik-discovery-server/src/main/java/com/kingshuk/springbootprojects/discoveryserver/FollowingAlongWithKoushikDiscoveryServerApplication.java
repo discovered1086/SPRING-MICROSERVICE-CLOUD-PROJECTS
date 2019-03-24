9a86c5e3ff1aabe59958ad2d0d63b8304a6c2ead
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-03-23 21:38:23 -0500

    Adding another discovery server

---
+++ following-along-with-koushik-discovery-server/src/main/java/com/kingshuk/springbootprojects/discoveryserver/FollowingAlongWithKoushikDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.kingshuk.springbootprojects.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowingAlongWithKoushikDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowingAlongWithKoushikDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ following-along-with-koushik-discovery-server/src/test/java/com/kingshuk/springbootprojects/discoveryserver/FollowingAlongWithKoushikDiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kingshuk.springbootprojects.discoveryserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FollowingAlongWithKoushikDiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
