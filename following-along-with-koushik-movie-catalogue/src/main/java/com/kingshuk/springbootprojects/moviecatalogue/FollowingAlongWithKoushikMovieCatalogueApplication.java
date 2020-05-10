67e352fdb46d33f08a84e0a1bd47fc31998ea4ce
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-05-09 19:05:11 -0500

    Added time out

--- following-along-with-koushik-movie-catalogue/src/main/java/com/kingshuk/springbootprojects/moviecatalogue/FollowingAlongWithKoushikMovieCatalogueApplication.java
+++ following-along-with-koushik-movie-catalogue/src/main/java/com/kingshuk/springbootprojects/moviecatalogue/FollowingAlongWithKoushikMovieCatalogueApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -15,13 +16,24 @@ public class FollowingAlongWithKoushikMovieCatalogueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FollowingAlongWithKoushikMovieCatalogueApplication.class, args);
 	}
-	
+
+	/*
+	 * @Bean
+	 * 
+	 * @LoadBalanced public RestTemplate getRestTemplate() { RestTemplateBuilder
+	 * restTemplateBuilder = new RestTemplateBuilder();
+	 * restTemplateBuilder.setConnectTimeout(Duration.ofMinutes(1l)); return
+	 * restTemplateBuilder.build(); }
+	 */
+
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setConnectTimeout(5000);
+		return new RestTemplate(requestFactory);
 	}
-	
+
 	@Bean
 	public WebClient.Builder getWebClient() {
 		return WebClient.builder();
--- following-along-with-koushik-movie-catalogue/src/main/java/com/kingshuk/springbootprojects/moviecatalogue/controller/CatalogueController.java
+++ following-along-with-koushik-movie-catalogue/src/main/java/com/kingshuk/springbootprojects/moviecatalogue/controller/CatalogueController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
