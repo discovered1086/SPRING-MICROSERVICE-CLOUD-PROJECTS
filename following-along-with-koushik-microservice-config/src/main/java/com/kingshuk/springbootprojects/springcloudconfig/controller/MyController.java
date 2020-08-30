35874f2fd5d584e9dc974e8092d1693207c8fcd2
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-08-30 18:45:22 -0500

    Exploring the power of @Value annotation

--- following-along-with-koushik-microservice-config/src/main/java/com/kingshuk/springbootprojects/springcloudconfig/controller/MyController.java
+++ following-along-with-koushik-microservice-config/src/main/java/com/kingshuk/springbootprojects/springcloudconfig/controller/MyController.java
@@ -1,18 +1,41 @@
 package com.kingshuk.springbootprojects.springcloudconfig.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-	@Value("${my.greetings}")
+	@Value("${my.greetings:Hello from the default value}")
 	private String greeting;
+	
+	@Value("Hello from hardcoded String")
+	private String greeting2;
+	
+	@Value("${my.list.value}")
+	private List<String> listOfStrings;
+	
+	@Value("#{${userLocation}}")
+	private Map<String, String> friendsLocations;
 
 	@GetMapping("/hello")
 	public String getGreeting() {
 		return greeting;
+	}
+	
+	@GetMapping(path = "/hello-list", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> getGreetingList() {
+		return listOfStrings;
+	}
+	
+	@GetMapping(path = "/hello-friends", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, String> getGreetingFriends() {
+		return friendsLocations;
 	}
 
 }
