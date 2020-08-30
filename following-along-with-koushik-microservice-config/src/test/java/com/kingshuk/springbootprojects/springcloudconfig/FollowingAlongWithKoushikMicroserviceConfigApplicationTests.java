45dee0770fead7f788cbc53e7469ebbcfb7adfee
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-08-30 12:15:47 -0500

    Microservice level 3 introduction

---
+++ following-along-with-koushik-microservice-config/src/test/java/com/kingshuk/springbootprojects/springcloudconfig/FollowingAlongWithKoushikMicroserviceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingshuk.springbootprojects.springcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowingAlongWithKoushikMicroserviceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
