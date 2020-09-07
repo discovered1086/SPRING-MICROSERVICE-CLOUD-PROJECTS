3dee94804259c6dcc378de1f7e0f1cb338306f69
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-09-06 21:41:43 -0500

    Configuration changes in the config server and the client

--- following-along-with-koushik-config-server/src/main/java/com/kingshuk/springbootprojects/springcloudconfig/server/FollowingAlongWithKoushikConfigServerApplication.java
+++ following-along-with-koushik-config-server/src/main/java/com/kingshuk/springbootprojects/springcloudconfig/server/FollowingAlongWithKoushikConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.kingshuk.springbootprojects.springcloudconfig.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
+@EnableConfigServer
 public class FollowingAlongWithKoushikConfigServerApplication {
 
 	public static void main(String[] args) {
